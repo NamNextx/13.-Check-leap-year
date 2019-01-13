a379c634b3c1ec136a3b8100818d068ddb4cefae
Namnextx <phuongnam.nextx@gmail.com>
2019-01-13 08:32:47 +0700

    1. Create file

---
+++ funcChange.js
@@ -0,0 +1,18 @@
+function changer() {
+
+    var year=parseInt(document.getElementById('idtext').value);
+
+    //var checkyear= year % 4 == 0;
+    if(year/4 == 0)
+    {
+        alert("phai nam");
+    }
+    else if(isNaN(NaN))
+    {
+        document.write("Bad")
+    }
+
+    else document.write(year/4);
+
+
+        }

8f2e810004bfef97d2857e9e081230c0c2a271ef
Namnextx <phuongnam.nextx@gmail.com>
2019-01-13 09:15:39 +0700

    2. Done back-end 3. Need try to fron-end

--- funcChange.js
+++ funcChange.js
@@ -1,18 +1,40 @@
+/*
+Cách xác định năm nhuận: Những năm chia hết cho 4 là năm nhuận, ngoại trừ những năm chia hết cho 100 mà không chia hết cho 400.
+
+Từ đó, có thể rút gọn thành các quy tắc xác định năm nhuận:
+
+- Những năm chia hết cho 4 mà không chia hết cho 100 là năm nhuận
+
+- Những năm chia hết cho 100 mà không chia hết cho 400 thì KHÔNG PHẢI là năm nhuận
+
+-Những năm chia hết đồng thời cho 100 và 400 là năm nhuận*/
+
+
 function changer() {
 
-    var year=parseInt(document.getElementById('idtext').value);
+    year=parseInt(document.getElementById('idtext').value);
+    if (!Number.isNaN(year)){
+        var flag=false;
+        var checkyear4= year % 4 == 0;
+        if(checkyear4) {
+            var checkyear100 = year % 100 == 0;
+            if (checkyear100) {
+                checkyear400 = year % 400 == 0;
+                if (checkyear400) {
+                    flag = true;
+                } else flag = false;
+            }
+            else flag=true;
+        }
 
-    //var checkyear= year % 4 == 0;
-    if(year/4 == 0)
-    {
-        alert("phai nam");
-    }
-    else if(isNaN(NaN))
-    {
-        document.write("Bad")
-    }
+        if(flag) {nhuộn
+            alert("Năm bạn vừa nhập là năm nhuon")
+        }
+        else document.write("Năm bạn vừa nhập không phải năm nhuộn ")
 
-    else document.write(year/4);
+    }
+    else alert("Can nhap dung dinh dang")
+
 
 
         }
